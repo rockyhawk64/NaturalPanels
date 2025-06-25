c861589c92b154bea480fc4c2bf7f2067e71f9c2
rockyhawk64 <rockyhawk99@gmail.com>
2025-06-26 03:16:04 +1000

    dialog implementation experiemental

---
+++ src/me/rockyhawk/naturalpanels/builder/ComponentBuilder.java
@@ -0,0 +1,71 @@
+package me.rockyhawk.naturalpanels.builder;
+
+import com.fancyinnovations.fancydialogs.api.data.DialogButton;
+import com.fancyinnovations.fancydialogs.api.data.inputs.DialogInputs;
+import com.fancyinnovations.fancydialogs.api.data.inputs.DialogTextField;
+import me.rockyhawk.naturalpanels.Context;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComponentBuilder {
+    private final Context ctx;
+
+    public ComponentBuilder(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    public List<DialogButton> buttonBuilder(Player player, YamlConfiguration config, List<String> buttonOrder){
+        List<DialogButton> buttons = new ArrayList<>();
+        // Iterate buttons for dialog
+        for(String entry : buttonOrder){
+            if(!config.contains("buttons." + entry)) continue;
+            ConfigurationSection buttonSection = config.getConfigurationSection("buttons." + entry);
+            String buttonLabel = buttonSection.getString("label");
+            String buttonTooltip = buttonSection.getString("tooltip");
+
+            // Create actions for dialog buttons
+            List<DialogButton.DialogAction> actions = new ArrayList<>();
+            DialogButton.DialogAction action = new DialogButton.DialogAction("run_command", "say hello");
+            actions.add(action);
+
+            // Add button into dialog
+            DialogButton button = new DialogButton(
+                    ctx.text.parseText(player, buttonLabel),
+                    ctx.text.parseText(player, buttonTooltip), actions);
+            buttons.add(button);
+        }
+        return buttons;
+    }
+
+    public DialogInputs inputBuilder(Player player, YamlConfiguration config, List<String> inputOrder){
+        List<DialogTextField> inputs = new ArrayList<>();
+        int order = 1;
+        for(String entry : inputOrder){
+            if(!config.contains("inputs." + entry)) continue;
+            ConfigurationSection inputSection = config.getConfigurationSection("inputs." + entry);
+
+            String inputKey = inputSection.getString("key");
+            String inputLabel = inputSection.getString("label");
+            String placeholder = inputSection.getString("placeholder");
+            int maxLength = inputSection.getInt("max-length");
+            int maxLines = inputSection.getInt("max-lines");
+
+            DialogTextField textField = new DialogTextField(
+                    ctx.text.parseText(player, inputKey),
+                    ctx.text.parseText(player, inputLabel),
+                    order,
+                    ctx.text.parseText(player, placeholder),
+                    maxLength,
+                    maxLines
+            );
+
+            inputs.add(textField);
+            order++;
+        }
+        return new DialogInputs(inputs);
+    }
+}
--- src/me/rockyhawk/naturalpanels/builder/PanelBuilder.java
+++ src/me/rockyhawk/naturalpanels/builder/PanelBuilder.java
@@ -1,21 +1,57 @@
 package me.rockyhawk.naturalpanels.builder;
 
 import com.fancyinnovations.fancydialogs.api.Dialog;
+import com.fancyinnovations.fancydialogs.api.FancyDialogs;
+import com.fancyinnovations.fancydialogs.api.data.DialogBodyData;
+import com.fancyinnovations.fancydialogs.api.data.DialogData;
 import me.rockyhawk.naturalpanels.Context;
 import me.rockyhawk.naturalpanels.panel.Panel;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PanelBuilder {
     private final Context ctx;
-    private final DialogFactory factory;
+    private final ComponentBuilder componentsBuilder;
 
     public PanelBuilder(Context ctx) {
         this.ctx = ctx;
-        factory = new DialogFactory(ctx);
+        componentsBuilder = new ComponentBuilder(ctx);
     }
 
     public void openPanel(Player p, Panel panel){
-        Dialog dialog = factory.constructDialog(p, panel);
+        Dialog dialog = constructDialog(p, panel);
         dialog.open(p);
+    }
+
+    private Dialog constructDialog(Player player, Panel panel) {
+        YamlConfiguration config = panel.getConfig();
+
+        String title = ctx.text.parseText(player, config.getString("title"));
+        boolean closable = config.getBoolean("closable");
+        List<String> text = config.getStringList("text");
+        List<String> inputOrder = config.getStringList("input-order");
+        List<String> buttonOrder = config.getStringList("button-order");
+
+        // Create body components
+        List<DialogBodyData> body = new ArrayList<>();
+        for(String entry : text){
+            DialogBodyData temp = new DialogBodyData(ctx.text.parseText(player, entry));
+            body.add(temp);
+        }
+
+        // Build the dialog
+        DialogData dialogData = new DialogData(
+                panel.getName(),
+                title,
+                closable,
+                body,
+                componentsBuilder.inputBuilder(player, config, inputOrder),
+                componentsBuilder.buttonBuilder(player, config, buttonOrder)
+        );
+
+        return FancyDialogs.get().createDialog(dialogData);
     }
 }
